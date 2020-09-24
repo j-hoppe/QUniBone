5073285f875a3fe63bd76d4af43ac7b92fbf9c14
Joerg Hoppe <j_hoppe@t-online.de>
2020-09-24 11:21:24 +0200

    Compile scripts

--- compile.sh
+++ compile.sh
@@ -12,14 +12,15 @@ MAKEFILE=makefile$PLATFORM_SUFFIX
 
 # Debugging: remote from Eclipse. Compile on BBB is release.
 export MAKE_CONFIGURATION=RELEASE
+export MAKE_QUNIBUS
 
 cd 10.03_app_demo/2_src
 
 if [ "$1" == "-a" ] ; then
-  make -f $MAKEFILE clean
+  make clean
 fi
 
-make -f $MAKEFILE
+make
 cd ~
 
 echo "To run binary, call"
