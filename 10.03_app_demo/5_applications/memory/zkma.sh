edbbdae6423c49cd3df4d54743aa9950a1093826
Joerg Hoppe <j_hoppe@t-online.de>
2020-09-29 19:39:47 +0200

    moved "ZKMA" app to "memory"

--- 10.03_app_demo/5_applications/memory/zkma.sh
+++ 10.03_app_demo/5_applications/memory/zkma.sh
@@ -1,4 +1,4 @@
 # start "demo" application, emulate memory, load "ZKMA",
-cd ~/10.03_app_demo/5_applications/zkma
+cd ~/10.03_app_demo/5_applications/memory
 ~/10.03_app_demo/4_deploy/demo --verbose --cmdfile zkma.cmd $*
 
